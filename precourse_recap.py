230c05a55fdd34acefcaf32cf91d0bf1afd88eb6
Eliza Peterson <kuipercat@gmail.com>
2023-01-09 15:52:09 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+meaning_of_life = 42
+good_book = "Hitchhikers Guide to the Galaxy"
+
+print("What is the meaning of life?")
+print(meaning_of_life)
+print("Where'd you hear that?")
+print(good_book)
+
+print("What's the biggest number you know?")
+print(meaning_of_life * 100000000000000000000000000)
+print("That's not the biggest...")
+print(meaning_of_life * 1000000000000000000000 * 10000000000000000000)
+print("Ok then...")
